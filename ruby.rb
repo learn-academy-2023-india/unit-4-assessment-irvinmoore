ca0a712cb09d86f18f279e8a0b4b1a6a30007aad
irvinmoore <irvin.moore7@gmail.com>
2024-01-21 21:56:27 -0800

    4

--- ruby.rb
+++ ruby.rb
@@ -5,6 +5,33 @@
 # Run the file with the following command: $ rspec ruby_spec.rb
 
 # Add comments to your code explaining your approach
-
-class Bike
-end
+ 
+  
+  #class Bike
+    #attr_accessor :real, :model, :wheels, :speed
+  
+    #def initialize(model)
+      #@real = true
+      #@model = model
+      #@wheels = 2
+      #@speed = 0
+    #end
+  
+    #def become_tricycle
+      #@wheels = 3
+    #end
+  
+    #def increase_speed(amount)
+      #@speed += amount
+    #end
+  
+    #def decrease_speed(amount)
+      #@speed -= amount
+      #@speed = 0 if @speed < 0
+    #end
+  
+    #def log_data
+      #"Model: #{@model}, Wheels: #{@wheels}, Speed: #{@speed}"
+    #end
+  #end
+  
